05bcd8e492ca5d51143cbe62cf2dbd9a7ffca776
ajlima12 <ajlimaa12@gmail.com>
2025-11-22 16:03:23 -0300

    adicionando cors e segurança

---
+++ src/main/java/com/anajulia/modulo21/configuracao/CorsPropriedades.java
@@ -0,0 +1,19 @@
+package com.anajulia.modulo21.configuracao;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "spring.cors")
+public class CorsPropriedades {
+    private List<String> url;
+
+    public List<String> getUrl() {
+        return url;
+    }
+
+    public void setUrl(List<String> url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/com/anajulia/modulo21/configuracao/RotasConfiguracao.java
@@ -0,0 +1,25 @@
+package com.anajulia.modulo21.configuracao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class RotasConfiguracao {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(
+                                "/anajulia/modulo2.2/login/**"
+                        )
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated()
+                ).oauth2ResourceServer(oauth -> oauth.jwt())
+                .build();
+    }
+}
